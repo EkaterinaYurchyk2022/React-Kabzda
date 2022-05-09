83a224bf859dea782119b8b53f512f1fd0e74232
EkaterinaYurchyk2022 <ekaterina.yurchyk.s@gmail.com>
2022-05-09 15:30:56 +0300

    "useEffect 22 lesson"

--- src/stories/UseEffect.stories.tsx
+++ src/stories/UseEffect.stories.tsx
@@ -42,3 +42,23 @@ export const SimpleExample = () => {
     </>
 }
 
+export const SetTimeoutExample = () => {
+    const [fake, setFake] = useState(1)
+    const [counter, setCounter] = useState(1)
+    console.log("SetTimeoutExample")
+
+
+    useEffect(() => {
+        setInterval(() => {
+            setCounter(state => state + 1)
+        }, 1000)
+    }, [])
+
+
+    return <>
+        Hello, counter: {counter} - fake: {fake}
+        {/* <button onClick={() => setFake(fake + 1)}>fake+</button>
+        <button onClick={() => setCounter(counter + 1)}>counter+</button>*/}
+    </>
+}
+
